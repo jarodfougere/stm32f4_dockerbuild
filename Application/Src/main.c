d3a48b76a041fdb4791e9b93ac00ed8349363364
CarlRim <carl.mattatall@rimot.io>
2020-01-28 12:10:26 -0400

    gahh

--- Application/Src/main.c
+++ Application/Src/main.c
@@ -5,11 +5,20 @@
 
 int main(void)
 {
+    /* initialize system clock */
+    rcc_init();
+
+    /* TODO : if returning from bootloader, execute DFU jump */
 
 
 
+    
 
-
+    /* application loop */
+    while(1)
+    {
+        
+    }
 
     return 0;
 }
