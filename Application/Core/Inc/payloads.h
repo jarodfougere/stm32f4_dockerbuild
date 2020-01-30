5064aac65607dcd4c39ac2d63c13ea88b482ec24
CarlRim <carl.mattatall@rimot.io>
2020-01-30 16:12:56 -0400

    for jarod love carl

--- Application/Core/Inc/payloads.h
+++ Application/Core/Inc/payloads.h
@@ -3,5 +3,63 @@
 #include <stdint.h>
 #include <time.h>
 
+#include "gpio_interface.h"
+#include "rf_interface.h"
+#include "humidity_interface.h"
+#include "temperature_interface.h"
+
+
+
+
+typedef struct
+{
+    int8_t  firmware_id[8];
+    int8_t  hardware_id[8];
+    int8_t  device_uid[13];
+    uint8_t num_inputs;
+    uint8_t num_relays;
+    uint8_t num_batteries;
+}   device_payload_t;
+
+typedef struct
+{
+    uint32_t id;
+    uint32_t type;
+    pin_config_t cfg;
+}   relay_payload_t;
+
+
+typedef struct
+{
+    uint32_t id;
+    uint32_t type;
+    pin_config_t cfg;
+    uint32_t  level;
+}   digital_input_payload_t;
+
+
+typedef struct
+{
+    uint32_t id;
+    uint32_t type;
+    pin_config_t cfg;
+    uint32_t voltage;
+}   battery_payload_t;
+
+
+
+
+typedef struct
+{
+    uint32_t humidity;
+    uint32_t celsius;
+}   moth_payload_t;
+
+
+
+typedef struct
+{
+
+} rf_payload_t;
 
 #endif
--- Application/Core/Src/payloads.c
+++ Application/Core/Src/payloads.c
@@ -1,4 +1,13 @@
 #include <string.h>
-#include <time.h>
+#include <time.h> /* eventually we want to add timestamps to payloads */
 
 #include "payloads.h"
+#include "system_config.h"
+
+
+const device_payload_t device_payload = 
+{
+    
+};
+
+
