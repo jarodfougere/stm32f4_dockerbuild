901c421e3ba1b09e665c329221712b10fc4ef6d1
CarlRim <carl.mattatall@rimot.io>
2020-01-31 12:25:06 -0400

    re-add test func

--- Application/Core/Src/main.c
+++ Application/Core/Src/main.c
@@ -7,9 +7,28 @@
 
 void main(void)
 {
-
     while(1)
     {
+        
     }
 }
 
+
+
+void testParse(void)
+{   
+    printf("please enter a JSON in the console.\n");
+    char msg[250];
+    if(NULL != gets(msg))
+    {   
+        printf("received data >%s<\n", msg);
+
+
+        // parse the tokens using jsmn
+
+        // do some stuff 
+
+        //get an idea for how JSMN works and make a plan to implement the data parsing module
+
+    }
+}
