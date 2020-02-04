84326109178bb68496ee0177ce8d5e1ff746c34e
CarlRim <carl.mattatall@rimot.io>
2020-02-04 17:34:25 -0400

    ADD PORTABLE SERIAL DATA MIDDLEWARE

--- Drivers/Peripherals/Inc/serial_data.h
+++ Drivers/Peripherals/Inc/serial_data.h
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdint.h>
+#include <stdarg.h>
 
 /* WARNING: NOT SURE IF IARCC SUPPORTS VA_ARGS */
 void transmit_serial(const char* restrict format, ...);
--- Drivers/Peripherals/Src/serial_data.c
+++ Drivers/Peripherals/Src/serial_data.c
@@ -7,18 +7,18 @@
 
 //Carl TODO: IMPLEMENT PSEUDO OVERLOAD USING WEAK ATTRIBUTE (BUT ADD IAR/GCC SYSTEM COMPAT)
 
+
 void transmit_serial(const char* restrict format, ...)
-{   
-    if(format != NULL)
-    {
-#if defined(LOCAL_SYSTEM)
-    printf(format, ...);
+{     
+    va_list args;
+    va_start(args, format);
+#if defined(MY_SYSTEM)
+    vprintf(format, args);
 #elif defined(TARGET_PROCESSOR)
     char data_buf[500];
-    snprintf(data_buf, sizeof(data_buf), format, ...);
+    vsnprintf(data_buf, sizeof(data_buf), format, args);
     send_data_via_peripheral((const int8_t*)data_buf, min(strlen(data_buf), sizeof(data_buf)));
 #else
 #   error NETHER >LOCAL_SYSTEM< OR >TARGET_PROCESSOR< HAVE BEEN DEFINED
 #endif
-    }
 }
