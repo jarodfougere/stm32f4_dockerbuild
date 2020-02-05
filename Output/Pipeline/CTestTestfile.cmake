fb2d0f33203c3c0e4f31ca6e393a971af8d59b4f
CarlRim <carl.mattatall@rimot.io>
2020-02-05 14:26:05 -0400

    purge pipelined files from origin

--- Output/Pipeline/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/cmatt/Documents/rimot-lowpower-sensorboard/CMake
-# Build directory: C:/Users/cmatt/Documents/rimot-lowpower-sensorboard/Output/Pipeline
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
