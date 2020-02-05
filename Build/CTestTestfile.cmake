fd2c169060fae6df8e38a1e07530cb25852d5bb7
CarlRim <carl.mattatall@rimot.io>
2020-02-05 09:26:15 -0400

    i hate git (carl)

--- Build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Admin/Desktop/Jarod/Dev/rimot-lowpower-sensorboard/rimot-lowpower-sensorboard
-# Build directory: C:/Users/Admin/Desktop/Jarod/Dev/rimot-lowpower-sensorboard/rimot-lowpower-sensorboard/Build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
