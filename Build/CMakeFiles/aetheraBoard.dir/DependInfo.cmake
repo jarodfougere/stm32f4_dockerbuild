16f35e75fec48392d251015d51fdf510c92956fa
jarod <jarod.fougere@rimot.io>
2020-02-05 09:24:12 -0400

    i hate everything (carl)

---
+++ Build/CMakeFiles/aetheraBoard.dir/DependInfo.cmake
@@ -0,0 +1,45 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "C:/Users/Admin/Desktop/Jarod/Dev/rimot-lowpower-sensorboard/rimot-lowpower-sensorboard/Application/Core/Src/gpio_interface.c" "C:/Users/Admin/Desktop/Jarod/Dev/rimot-lowpower-sensorboard/rimot-lowpower-sensorboard/Build/CMakeFiles/aetheraBoard.dir/Application/Core/Src/gpio_interface.c.obj"
+  "C:/Users/Admin/Desktop/Jarod/Dev/rimot-lowpower-sensorboard/rimot-lowpower-sensorboard/Application/Core/Src/main.c" "C:/Users/Admin/Desktop/Jarod/Dev/rimot-lowpower-sensorboard/rimot-lowpower-sensorboard/Build/CMakeFiles/aetheraBoard.dir/Application/Core/Src/main.c.obj"
+  "C:/Users/Admin/Desktop/Jarod/Dev/rimot-lowpower-sensorboard/rimot-lowpower-sensorboard/Application/Core/Src/payloads.c" "C:/Users/Admin/Desktop/Jarod/Dev/rimot-lowpower-sensorboard/rimot-lowpower-sensorboard/Build/CMakeFiles/aetheraBoard.dir/Application/Core/Src/payloads.c.obj"
+  "C:/Users/Admin/Desktop/Jarod/Dev/rimot-lowpower-sensorboard/rimot-lowpower-sensorboard/Application/Core/Src/rf_interface.c" "C:/Users/Admin/Desktop/Jarod/Dev/rimot-lowpower-sensorboard/rimot-lowpower-sensorboard/Build/CMakeFiles/aetheraBoard.dir/Application/Core/Src/rf_interface.c.obj"
+  "C:/Users/Admin/Desktop/Jarod/Dev/rimot-lowpower-sensorboard/rimot-lowpower-sensorboard/Application/Core/Src/serialcommands.c" "C:/Users/Admin/Desktop/Jarod/Dev/rimot-lowpower-sensorboard/rimot-lowpower-sensorboard/Build/CMakeFiles/aetheraBoard.dir/Application/Core/Src/serialcommands.c.obj"
+  "C:/Users/Admin/Desktop/Jarod/Dev/rimot-lowpower-sensorboard/rimot-lowpower-sensorboard/Application/Core/Src/system_config.c" "C:/Users/Admin/Desktop/Jarod/Dev/rimot-lowpower-sensorboard/rimot-lowpower-sensorboard/Build/CMakeFiles/aetheraBoard.dir/Application/Core/Src/system_config.c.obj"
+  "C:/Users/Admin/Desktop/Jarod/Dev/rimot-lowpower-sensorboard/rimot-lowpower-sensorboard/Application/Port/Src/interrupts.c" "C:/Users/Admin/Desktop/Jarod/Dev/rimot-lowpower-sensorboard/rimot-lowpower-sensorboard/Build/CMakeFiles/aetheraBoard.dir/Application/Port/Src/interrupts.c.obj"
+  "C:/Users/Admin/Desktop/Jarod/Dev/rimot-lowpower-sensorboard/rimot-lowpower-sensorboard/Drivers/Peripherals/Src/serial_data.c" "C:/Users/Admin/Desktop/Jarod/Dev/rimot-lowpower-sensorboard/rimot-lowpower-sensorboard/Build/CMakeFiles/aetheraBoard.dir/Drivers/Peripherals/Src/serial_data.c.obj"
+  "C:/Users/Admin/Desktop/Jarod/Dev/rimot-lowpower-sensorboard/rimot-lowpower-sensorboard/Middleware/Src/jsmn.c" "C:/Users/Admin/Desktop/Jarod/Dev/rimot-lowpower-sensorboard/rimot-lowpower-sensorboard/Build/CMakeFiles/aetheraBoard.dir/Middleware/Src/jsmn.c.obj"
+  "C:/Users/Admin/Desktop/Jarod/Dev/rimot-lowpower-sensorboard/rimot-lowpower-sensorboard/Middleware/Src/leds.c" "C:/Users/Admin/Desktop/Jarod/Dev/rimot-lowpower-sensorboard/rimot-lowpower-sensorboard/Build/CMakeFiles/aetheraBoard.dir/Middleware/Src/leds.c.obj"
+  "C:/Users/Admin/Desktop/Jarod/Dev/rimot-lowpower-sensorboard/rimot-lowpower-sensorboard/Middleware/Src/parsing.c" "C:/Users/Admin/Desktop/Jarod/Dev/rimot-lowpower-sensorboard/rimot-lowpower-sensorboard/Build/CMakeFiles/aetheraBoard.dir/Middleware/Src/parsing.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "MY_SYSTEM"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../Application/Core/Inc"
+  "../Application/Port/Inc"
+  "../Application/Port"
+  "../Drivers"
+  "../Drivers/CMSIS/Core/Include"
+  "../Drivers/CMSIS/Device/ST/*/Include"
+  "../Drivers/Peripherals"
+  "../Drivers/Peripherals/Inc"
+  "../Middleware"
+  "../Middleware/Inc"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Build/CMakeFiles/aetheraBoard.dir/cmake_clean.cmake
@@ -0,0 +1,22 @@
+file(REMOVE_RECURSE
+  "../Binaries/aetheraBoard.exe"
+  "../Binaries/aetheraBoard.exe.manifest"
+  "../Binaries/aetheraBoard.pdb"
+  "../Binaries/libaetheraBoard.dll.a"
+  "CMakeFiles/aetheraBoard.dir/Application/Core/Src/gpio_interface.c.obj"
+  "CMakeFiles/aetheraBoard.dir/Application/Core/Src/main.c.obj"
+  "CMakeFiles/aetheraBoard.dir/Application/Core/Src/payloads.c.obj"
+  "CMakeFiles/aetheraBoard.dir/Application/Core/Src/rf_interface.c.obj"
+  "CMakeFiles/aetheraBoard.dir/Application/Core/Src/serialcommands.c.obj"
+  "CMakeFiles/aetheraBoard.dir/Application/Core/Src/system_config.c.obj"
+  "CMakeFiles/aetheraBoard.dir/Application/Port/Src/interrupts.c.obj"
+  "CMakeFiles/aetheraBoard.dir/Drivers/Peripherals/Src/serial_data.c.obj"
+  "CMakeFiles/aetheraBoard.dir/Middleware/Src/jsmn.c.obj"
+  "CMakeFiles/aetheraBoard.dir/Middleware/Src/leds.c.obj"
+  "CMakeFiles/aetheraBoard.dir/Middleware/Src/parsing.c.obj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/aetheraBoard.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
