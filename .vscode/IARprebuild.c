0ac53e3abd4afcd1e38a3bfe6265fa7c73912511
CarlRim <carl.mattatall@rimot.io>
2020-05-04 12:11:35 -0300

    add automatic update of settings.json::iarconfig

--- .vscode/IARprebuild.c
+++ .vscode/IARprebuild.c
@@ -25,15 +25,12 @@ int main(void)
     FILE *fSettings = NULL;
     FILE *fTemp = NULL;
     FILE *fIAR = NULL;
-    const char settingsFile[] = ".vscode/settings.json";
-    const char iarFile[] = ".vscode/iar-vsc.json";
+    const char settingsFile[] = "settings.json";
+    const char iarFile[] = "iar-vsc.json";
     const char keyToFindinIARfile[] = "configuration";
     const char keyToFindinSettingsFile[] = "iarvsc.configuration";
     char valueOfKeyFromIARfile[100] = {0};
     char buffer[1000];
-
-
-
 
     /*  Open all required files */
     fSettings  = fopen(settingsFile, "r");
@@ -43,37 +40,28 @@ int main(void)
     /* fopen() return NULL if unable to open file in given mode. */
     if (fSettings == NULL || fTemp == NULL || fIAR == NULL)
     {
-        /* Unable to open file hence exit */
+        /* Unable to open file so exit */
         printf("\nUnable to open a file.\n");
         printf("Please check whether file exists and you have read/write privilege.\n");
         exit(EXIT_SUCCESS);
     }
 
-    printf("reading lines from %s....\n", iarFile);
     unsigned line = 0;  /* line counter in file */
     while(NULL != fgets(buffer, sizeof(buffer), fIAR))
     {   
-        printf("%d : %s", line++, buffer);
         char *ptr = strstr(buffer, keyToFindinIARfile);
         if(NULL != ptr) /* if the key is on the current line */
         {
 
-            printf("\n\n\nfound %s in line : %d of %s\n", 
-            keyToFindinIARfile,
-            line,
-            iarFile);
-
             /* Advance to end of word */
             while('\"' != *ptr)
             {
-                printf("read character : >%c<\n", *ptr);
                 ptr++;
             }
 
             /* Skip whitespace */
             do
             {
-                printf("read character : >%c<\n", *ptr);
                 ptr++;
             }   while(isspace(*ptr));
             
@@ -81,12 +69,11 @@ int main(void)
             /* Key : val delimiter */
             if(':' == *ptr)
             {
-                printf("FOUND COLON\n");
+                //printf("FOUND COLON\n");
                 ptr++;
             }
             else
             {
-                printf("ERROR : JSON FORMAT in %s\n", iarFile);
                 fclose(fSettings);
                 fclose(fTemp);
                 fclose(fIAR);
@@ -96,7 +83,6 @@ int main(void)
             /* Skip whitespace */
             do
             {
-                printf("read character : >%c<\n", *ptr);
                 ptr++;
             }   while(isspace(*ptr));
 
@@ -104,11 +90,13 @@ int main(void)
             if('\"' == *ptr)
             {
                 ptr++;
-                int i = 0;
+                unsigned i = 0;
                 if(0 == ptr[i])
                 {
-                    printf("ERROR : EMPTY VALUE FOR KEY %s\n", 
-                    keyToFindinIARfile);
+                    fclose(fSettings);
+                    fclose(fTemp);
+                    fclose(fIAR);
+                    exit(EXIT_FAILURE);
                 }
                 else
                 {
@@ -119,7 +107,6 @@ int main(void)
                         /* value can't fit into value holder */
                         if(i > sizeof(valueOfKeyFromIARfile))
                         {
-                            printf("ERROR : VALUE BUFFER SIZE\n");
                             fclose(fSettings);
                             fclose(fTemp);
                             fclose(fIAR);
@@ -132,7 +119,6 @@ int main(void)
                          */
                         else if(i > (sizeof(buffer) - (ptr - buffer)))
                         {
-                            printf("ERROR : LINE BUFFER SIZE\n");
                             fclose(fSettings);
                             fclose(fTemp);
                             fclose(fIAR);
@@ -143,17 +129,14 @@ int main(void)
                             /* Copy byte */
                             valueOfKeyFromIARfile[i] = ptr[i];
                             i++;
-                            printf("copied >%c<\n", valueOfKeyFromIARfile[i]);
                         }
                     }
                     i++;
                     valueOfKeyFromIARfile[i] = '\0'; /* Nul terminate */
-                    printf("KEY VALUE == %s\n", valueOfKeyFromIARfile);
                 }
             }
             else
             {
-                printf("ERROR : JSON FORMAT in %s\n", iarFile);
                 fclose(fSettings);
                 fclose(fTemp);
                 fclose(fIAR);
@@ -170,58 +153,31 @@ int main(void)
         if(NULL != ptr)
         {
             /* FOUND THE KEY IN SETTINGS FILE */
-            printf("found >\"%s\"< in %s\n", keyToFindinSettingsFile, settingsFile);
-
-            printf("buffer = %s\n", buffer);
-            
             char *t = buffer;
             while(t != (ptr - 1))
             {
                 fputc(*t, fTemp);
-                printf("wrote >%c<\n", *t);
                 t++;
             }
 
             /* CONFIRM KEY STARTS WITH QUOTE */
             if('\"' != *t)
             {   
-                printf("t = >%c<\n", *t);
-                printf("DID NOT FIND EXPECTED QUOTE AT START OF KEY\n");
-                printf("ERROR : JSON FORMAT in %s\n", settingsFile);
+                fclose(fSettings);
+                fclose(fTemp);
+                fclose(fIAR);
+                exit(EXIT_FAILURE);
             }
 
             char *max_t = ptr + strlen(keyToFindinSettingsFile);
             for(; t < max_t; t++)
             {
                 fputc(*t, fTemp);
-                printf("wrote >%c<\n", *t);
             }
             
             /* CONFIRM KEY ENDS WITH QUOTE */
             if('\"' != *t)
             {   
-                printf("t = >%c<\n", *t);
-                printf("DID NOT FIND EXPECTED QUOTE AT END OF KEY\n");
-                printf("ERROR : JSON FORMAT in %s\n", settingsFile);
-            }
-            else
-            {
-                fputc(*t, fTemp);
-            }
-
-            /* Skip whitespace */
-            t++;
-            while(isspace(*t))
-            {
-                fputc(*t, fTemp);
-                printf("wrote >%c<\n", *t);
-                t++;
-            }
-
-            if(':' != *t)
-            {   
-                printf("DID NOT FIND EXPECTED COLON. *t = >%c<\n", *t);
-                printf("ERROR : JSON FORMAT in %s\n", settingsFile);
                 fclose(fSettings);
                 fclose(fTemp);
                 fclose(fIAR);
@@ -230,7 +186,6 @@ int main(void)
             else
             {
                 fputc(*t, fTemp);
-                printf("wrote >%c<\n", *t);
             }
 
             /* Skip whitespace */
@@ -238,57 +193,121 @@ int main(void)
             while(isspace(*t))
             {
                 fputc(*t, fTemp);
-                printf("wrote >%c<\n", *t);
+                t++;
+            }
+
+            if(':' != *t)
+            {   
+                fclose(fSettings);
+                fclose(fTemp);
+                fclose(fIAR);
+                exit(EXIT_FAILURE);
+            }
+            else
+            {
+                fputc(*t, fTemp);
+            }
+
+            /* Skip whitespace */
+            t++;
+            while(isspace(*t))
+            {
+                fputc(*t, fTemp);
                 t++;
             }
 
             /* Find start of quoted key value */
             if('\"' != *t)
             {   
-                printf("DID NOT FIND EXPECTED QUOTE\n");
-                printf("ERROR : JSON FORMAT in %s\n", settingsFile);
                 fclose(fSettings);
                 fclose(fTemp);
                 fclose(fIAR);
                 exit(EXIT_FAILURE);
             }
+
             else
             {   
+
                 /* We copy the new value for IAR.config into the file */
-                printf("FOUND START OF VALUE\n");
                 fputc(*t, fTemp);
-                printf("wrote >%c<\n", *t);
                 t++;
 
 
-                printf("Remaining line buffer == >%s<\n", t);
-                
-                /* 
-                 * I don't trust stdlib / string / file 
-                 * functions not to append a nul char on my 
-                 * bytestring so I'm manually placing the bytes
-                 * into the file
-                 */
-                unsigned i;
-                const unsigned max = strlen(valueOfKeyFromIARfile);
-                for(i = 0; i < max; i++)
-                {   
-                    fputc(valueOfKeyFromIARfile[i], fTemp);
-                    printf("wrote >%c<\n", valueOfKeyFromIARfile[i]);
+
+                char *fwdCheck;
+                for(fwdCheck = t; *fwdCheck != '\"'; fwdCheck++)
+                {
+                    if(*fwdCheck == '\n' || *fwdCheck == '\0')
+                    {
+                        fclose(fSettings);
+                        fclose(fTemp);
+                        fclose(fIAR);
+                        exit(EXIT_FAILURE);
+                    }
+                }
+
+                unsigned oldValLen = (unsigned)(fwdCheck - t);
+                unsigned newValLen = strlen(valueOfKeyFromIARfile);
+
+
+                unsigned flag = 0;
+                unsigned k;
+                for(k = 0; k < newValLen; k++)
+                {
+                    fputc(valueOfKeyFromIARfile[k], fTemp);
+
+                    if((t != fwdCheck) && (flag == 0))
+                    {
+                        t++;
+                    }
+                    else
+                    {
+                        flag = 1;
+                    }
                 }
 
                 fputc('\"', fTemp);
-                printf("wrote >%c<\n", '\"');
-
+                fputc(',', fTemp);
                 
+
+                if(!flag)
+                {   
+                    //printf("fastwordwarding to end of old line\n");
+                    /* 
+                     * we need  to fast forward to end of
+                     * value in old line 
+                     */
+                    
+                    /* find delimiting comma in old line */
+                    char *fwdCheckCommaDelim = fwdCheck;
+                    for(; *fwdCheckCommaDelim != '\0'; fwdCheckCommaDelim++)
+                    {
+                        if(*fwdCheckCommaDelim == ',')
+                        {   
+                            break;
+                        }
+                    }
+                    
+                    /* 
+                     * We've alrady tacked on our delimiting comma in
+                     * new file but we need to make sure that
+                     * copying the rest of the file doesn't copy
+                     * the old comma (there would be 2 commas copied)
+                     * which is invalid JSON
+                     */
+
+                    /* 
+                     * Write spaces to file until we've passed
+                     * the delimiting comma in old file 
+                     */
+                    while(t < fwdCheck)
+                    {   
+                        fputc(' ', fTemp);
+                        t++;
+                    }
+                }
             }
 
-            while('\0' != *t)
-            {
-                fputc(*t, fTemp);
-                printf("wrote >%c<\n", *t);
-                t++;
-            }
         }
         else
         {   
@@ -297,11 +316,7 @@ int main(void)
              * to output file 
              */
             fputs(buffer, fTemp);
-            printf("wrote line %s\n", buffer);
         }
-
-
-        break;
     }
 
     /* Close all files to release resource */
@@ -309,15 +324,11 @@ int main(void)
     fclose(fTemp);
     fclose(fIAR);
 
-
-    #if 0
     /* Delete original source file */
     remove(settingsFile);
 
     /* Rename temp file as original file */
     rename("replace.tmp", settingsFile);
-
-    #endif
 
     return 0;
 }
