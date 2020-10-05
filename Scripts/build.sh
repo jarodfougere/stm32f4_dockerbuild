f55ef8263c8022ffd9c09aa0927b004ef890e71a
Carl Mattatall <cmattatall2@gmail.com>
2020-10-05 12:00:56 -0300

    cleanup more of the python script

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 #
 # BASH SCRIPT TO INITIATE CMAKE BUILD OF THE PROJECT
-#
-
 
 OUTPUT_DIR="bin"
 BUILD_TYPE="Debug"
 REBUILD=0
 
+HSE_VALUE=8000000 # units in HZ
+
 # Supported command line arguments
 # o : output directory name (default is bin)
 #
 # m : build configuration (default is debug)
+#
+# m : clean rebuild (CMake cache gets deleted)
 while getopts ":o:m:r" opt; do
     case ${opt} in
     o)  
@@ -44,7 +46,7 @@ else
 fi
 
 pushd objects > /dev/null
-cmake -S ../Source_Code -DCMAKE_TOOLCHAIN_FILE="arm-none-eabi-toolchain.cmake" -DDEVICE_MPN="STM32F411xE"  -DHSE_VALUE="8000000" -DHAL_DRIVER_CONFIG="DISABLED" -DCMAKE_SYSROOT="arm-none-eabi-9-2019-q4-major/arm-none-eabi" -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DOUTPUT_DIR_NAME=${OUTPUT_DIR}
+cmake -S ../Source_Code -DCMAKE_TOOLCHAIN_FILE="arm-none-eabi-toolchain.cmake" -DDEVICE_MPN="STM32F411xE"  -DHSE_VALUE="$HSE_VALUE" -DHAL_DRIVER_CONFIG="DISABLED" -DCMAKE_SYSROOT="arm-none-eabi-9-2019-q4-major/arm-none-eabi" -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DOUTPUT_DIR_NAME=${OUTPUT_DIR}
 popd > /dev/null
 cmake --build objects
 rm -r objects
--- docker_build.py
+++ docker_build.py
@@ -3,16 +3,11 @@ import subprocess
 import sys
 import argparse
 
-def argfmt(string):
+def argfmt(string): # this is literally just some space padding
     return str(" " + str(string) + " ")
-
-
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="parse command line args for docker build script")
-
-
-
     parser.add_argument("--output", action="store", dest="output_dir", default="bin")
     parser.add_argument("--mode", action="store", dest="build_mode", default="Debug")
     parser.add_argument("--rebuild", action="store_true", dest="rebuild")
@@ -23,13 +18,11 @@ if __name__ == "__main__":
     build_args += argfmt(" --build-arg rebuild=-r")
     docker_build_string = "docker build . "
     docker_build_string += build_args
-    print("docker_build_string = \"" + str(docker_build_string) + "\"\n")
     os.system(docker_build_string)
     image = str(subprocess.check_output("docker image ls -q | head -n 1", shell=True, universal_newlines=True))
-    # remove trailing newline / linefeed / carriage return in a platform independent manner
-    image = str(image.rstrip())
-    temp_container=str("dummy")
-    copy_string=str(temp_container) + ":/work/" + str(args.output_dir) + " ./"
-    os.system("docker create -ti --name" + " " + str(temp_container) + " " + str(image) + " " + "bash")
+    image = str(image.rstrip()) # remove trailing newline / linefeed / carriage returns
+    temp_container_name=str("dummy")
+    copy_string=str(temp_container_name) + ":/work/" + str(args.output_dir) + " ./"
+    os.system("docker create -ti --name" + " " + str(temp_container_name) + " " + str(image) + " " + "bash")
     os.system("docker cp" + " " + copy_string)
-    os.system("docker rm -f" + " " + str(temp_container))
+    os.system("docker rm -f" + " " + str(temp_container_name))
