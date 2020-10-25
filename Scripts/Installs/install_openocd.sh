325632cb5021d6cb9b4dab55ba6845f33a30bcbe
Carl Mattatall <cmattatall2@gmail.com>
2020-10-25 09:02:39 -0300

    update openocd install script to configure search directories for configuration files

--- Scripts/Installs/install_openocd.sh
+++ Scripts/Installs/install_openocd.sh
@@ -31,21 +31,6 @@ if [ $? -eq 0 ]; then
     exit 0
 else
     pushd /usr/local > /dev/null
-    ######################################################################
-    # I was playing around with build support for cmsis dap 
-    # (compilnig openocd with #./configure --enable-cmsis-dap && make)
-    # but I couldn't get the hidapi dependencies to build reliably.
-    # perhaps we can revisit this in the future
-    ######################################################################
-    # git clone git://github.com/Dashlane/hidapi.git
-    # pushd hidapi > /dev/null
-    # ./bootstrap
-    # ./configure --enable-static --disable-shared
-    # make clean && make
-    # export HIDAPI_LIBS="-L$HIDAPI_DIR/mac/.libs/ -lhidapi"
-    # export PKG_CONFIG_PATH="$HIDAPI_DIR/mac/pc/"
-    # popd > /dev/null
-    ######################################################################
     git clone https://github.com/ntfreak/openocd
     pushd openocd > /dev/null
     ./bootstrap
@@ -60,14 +45,13 @@ else
         echo "Error configuring openocd!"
         exit 1
     fi
-
-    make
+    make clean
+    make -j$(nproc)
+    make install -j$(nproc)
     if [ $? -ne 0 ]; then
         echo "Error installing openocd"
         exit 1
     fi
-    ln -s /usr/local/openocd/src/openocd /usr/local/bin/openocd
-    echo "export PATH=$PATH:/usr/local/openocd/tcl/" >> ~/.bashrc
     popd > /dev/null # leave /usr/local/openocd
 fi
 popd > /dev/null # leave /usr/local
